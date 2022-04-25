17d6dcc575edbc2fc5e858b7dd61332d0f3f71a0
Adriano <kdr@outlook.com.br>
2022-04-25 11:32:04 -0300

    add sequelize

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,30 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.status(200).json({
-      tudoCerto: true,
-    });
+  async index(req, res) {
+    try {
+      const novoAluno = await Aluno.create({
+        nome: 'Adriano',
+        sobrenome: 'Silveira',
+        email: 'adriano@gmail.com',
+        idade: 23,
+        peso: 85,
+        altura: 1.92,
+      });
+      res.status(200).json({
+        status: 'success',
+        data: {
+          aluno: novoAluno,
+        },
+      });
+    } catch (error) {
+      res.status(404).json({
+        status: 'fail',
+        data: {
+          error,
+        },
+      });
+    }
   }
 }
 
